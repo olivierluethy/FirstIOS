7b96fa3e3ea587ca261cb4eeb621cda6c1b9a762
BaskLash <olivier.luethy@gmx.net>
2026-03-31 23:33:38 +0200

    Changed UI for Month overlay

---
+++ PhotoSwiper/PhotoSwiper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoSwiper
+//
+//  Created by Olivier Luethy on 31.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoSwiper/PhotoSwiper/PhotoSwiperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSwiperApp.swift
+//  PhotoSwiper
+//
+//  Created by Olivier Luethy on 31.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct PhotoSwiperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
